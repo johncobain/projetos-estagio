4ab3838e0831a131fee42df8dee6eaa455309ebd
Oleeveira <octo20048@gmail.com>
2024-10-09 11:26:14 -0300

    blender bot test

--- whatBot/app.py
+++ whatBot/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-# from bot import BlenderBot
+from bot import BlenderBot
 
 app = Flask(__name__)
 
-# chat_bot = BlenderBot()
+chat_bot = BlenderBot()
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -12,12 +12,14 @@ def sms_reply():
     try:   
         message_body = request.form['Body'].lower()
         print(message_body)
-        if message_body == "hi" or message_body == "hello":
-            resp.message("Hello, how can I help you?")
-        elif message_body == 'bye':
-            resp.message("See u later!")    
-        else:      
-            resp.message("Sorry, I didn't undertand.")
+        response = chat_bot.get_response(message_body)
+        resp.message(response)
+        # if message_body == "hi" or message_body == "hello":
+        #     resp.message("Hello, how can I help you?")
+        # elif message_body == 'bye':
+        #     resp.message("See u later!")    
+        # else:      
+        #     resp.message("Sorry, I didn't undertand.")
     except Exception as e:
         resp.message(f"Oh no, I'm sorry! Error:{e}")
     return str(resp)
